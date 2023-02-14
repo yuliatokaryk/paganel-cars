7f40c1c55754c4c329e6c56eece19fd96f6566c1
yuliatokaryk <julia4099@gmail.com>
2023-02-14 10:36:05 +0200

    views counter rspec

---
+++ spec/services/cars_manager/views_counter_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe CarsManager::ViewsCounter do
+  let(:car) { create(:car) }
+
+  it 'returns car with the number of views will increased by 1' do
+    expect { described_class.new(car).call }.to change(car, :views).by(1)
+  end
+end
