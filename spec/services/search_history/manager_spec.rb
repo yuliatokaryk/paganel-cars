cb40cf0dd1e828c8451e2d26c0c6019a91ec08e2
yuliatokaryk <julia4099@gmail.com>
2023-02-16 13:04:13 +0200

    my search manager

---
+++ spec/services/search_history/manager_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe SearchHistory::Manager do
+  let(:params) { { make: 'x', model: 'x', year_to: 2010 } }
+  let(:user) { create(:user) }
+
+  it 'returns new record to database' do
+    expect { described_class.new(params: params, user: user.id).call }.to change(MySearch, :count)
+  end
+end
