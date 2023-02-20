ae3e5c891d062fa54c95c68d557549d30e63ad5b
yuliatokaryk <julia4099@gmail.com>
2023-02-20 17:22:09 +0200

    statistics controller spec

--- spec/controllers/statistics_controller_spec.rb
+++ spec/controllers/statistics_controller_spec.rb
@@ -3,25 +3,46 @@
 require 'rails_helper'
 
 RSpec.describe StatisticsController do
-  let(:user) { create(:user, :admin) }
+  let(:admin) { create(:user, :admin) }
+  let(:user) { create(:user) }
 
   describe 'GET index' do
-    it 'renders the index template' do
-      sign_in user
+    context 'when user is an admin' do
+      it 'renders the index template' do
+        sign_in admin
 
-      get :index
-      expect(response).to render_template('index')
+        get :index
+        expect(response).to render_template('index')
+      end
+
+      it 'has a 200 status code' do
+        sign_in admin
+
+        get :index
+        expect(response).to have_http_status(:ok)
+      end
     end
 
-    it 'has a 200 status code' do
-      sign_in user
+    context 'when user is not an admin' do
+      it 'redirect to the root template' do
+        sign_in user
 
-      get :index
-      expect(response).to have_http_status(:ok)
+        get :index
+        expect(response).to redirect_to('/')
+      end
+
+      it 'has a 302 status code' do
+        sign_in user
+
+        get :index
+        expect(response).to have_http_status(:found)
+      end
     end
   end
 
   describe 'DELETE destroy_all' do
+    let(:statistic) { create(:statistic) }
+
     it 'clears statistics' do
       delete :destroy_all
       expect(Statistic.count).to be(0)
