88983bd4737bbf14289ce8a0a1f693c71398c1e1
yuliatokaryk <julia4099@gmail.com>
2023-02-22 12:06:19 +0200

    statistics spec

--- spec/services/statistics/manager_spec.rb
+++ spec/services/statistics/manager_spec.rb
@@ -3,22 +3,24 @@
 require 'rails_helper'
 
 describe Statistics::Manager do
-  context 'when there is no record with the specified parameters yet' do
-    let(:params) { { make: 'x', model: 'x', year_to: 2010 } }
-    let(:total_quantity) { 3 }
+  describe '.call' do
+    context 'when there is no record with the specified parameters yet' do
+      let(:params) { { make: 'x', model: 'x', year_to: 2010 } }
+      let(:total_quantity) { 3 }
 
-    it 'returns new statistic record to database' do
-      expect { described_class.new(total_quantity: total_quantity, params: params).call }.to change(Statistic, :count)
+      it 'returns new statistic record to database' do
+        expect { described_class.new(total_quantity: total_quantity, params: params).call }.to change(Statistic, :count)
+      end
     end
-  end
 
-  context 'when there is record with the specified parameters' do
-    let(:statistic_record) { create(:statistic) }
-    let(:params) { { make: 'x', model: 'x' } }
-    let(:total_quantity) { 0 }
+    context 'when there is record with the specified parameters' do
+      let(:statistic_record) { create(:statistic) }
+      let(:params) { { make: 'x', model: 'x' } }
+      let(:total_quantity) { 0 }
 
-    it 'returns increase the requests quantity' do
-      expect { described_class.new(total_quantity: total_quantity, params: params).call }.to change(Statistic, :last)
+      it 'returns increase the requests quantity' do
+        expect { described_class.new(total_quantity: total_quantity, params: params).call }.to change(Statistic, :last)
+      end
     end
   end
 end
