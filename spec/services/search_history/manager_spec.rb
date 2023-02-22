b66fa6fc20ae910f2a42904dea33d0723ae6d195
yuliatokaryk <julia4099@gmail.com>
2023-02-22 12:06:11 +0200

    search history spec

--- spec/services/search_history/manager_spec.rb
+++ spec/services/search_history/manager_spec.rb
@@ -6,7 +6,9 @@ describe SearchHistory::Manager do
   let(:params) { { make: 'x', model: 'x', year_to: 2010 } }
   let(:user) { create(:user) }
 
-  it 'returns new record to database' do
-    expect { described_class.new(params: params, user: user.id).call }.to change(MySearch, :count)
+  describe '.call' do
+    it 'returns new record to database' do
+      expect { described_class.new(params: params, user: user.id).call }.to change(MySearch, :count)
+    end
   end
 end
