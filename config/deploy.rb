5a201aca717e1b084b7a62c10eecee4679df9cd5
yuliatokaryk <julia4099@gmail.com>
2023-02-27 11:07:39 +0200

    fix deploy storage

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :repo_url, 'git@github.com:yuliatokaryk/paganel-cars.git'
 
 set :deploy_to, "/home/deploy/#{fetch :application}"
 
-append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'public/uploads'
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'public/uploads', 'storage'
 
 set :keep_releases, 1
 
