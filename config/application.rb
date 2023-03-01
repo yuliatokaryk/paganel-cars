3cd1e202eebf159f6783e461a175c837e7bfecbc
yuliatokaryk <julia4099@gmail.com>
2023-03-01 18:50:19 +0200

    added interactor

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ require 'action_mailbox/engine'
 require 'action_text/engine'
 require 'action_view/railtie'
 require 'action_cable/engine'
+require 'interactor'
 # require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
