fe8c88043c15a50c10f30fae6e619794e83cfe04
yuliatokaryk <julia4099@gmail.com>
2023-03-02 11:16:37 +0200

    fixed sorter

--- app/interactors/cars/sorter.rb
+++ app/interactors/cars/sorter.rb
@@ -5,17 +5,7 @@ module Cars
     include Interactor
 
     def call
-      @cars = context.cars
-      @sort_by = context.sort_by
-      @sort_direction = context.sort_direction
-
-      sort_cars
-    end
-
-    private
-
-    def sort_cars
-      context.cars = @cars.order("#{@sort_by} #{@sort_direction}")
+      context.cars = context.cars.order("#{context.sort_by} #{context.sort_direction}")
     end
   end
 end
